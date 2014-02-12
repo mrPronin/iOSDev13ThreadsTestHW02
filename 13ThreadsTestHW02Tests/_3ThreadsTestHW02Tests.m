b8e3a5fbf1d6a6100a236d0fe7021959dcc5b7e6
Pronin Alexander <pronin.alx@gmail.com>
2014-02-12 21:16:22 +0200

    Initial Commit

---
+++ 13ThreadsTestHW02Tests/_3ThreadsTestHW02Tests.m
@@ -0,0 +1,34 @@
+//
+//  _3ThreadsTestHW02Tests.m
+//  13ThreadsTestHW02Tests
+//
+//  Created by Aleksandr Pronin on 12.02.14.
+//  Copyright (c) 2014 Aleksandr Pronin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _3ThreadsTestHW02Tests : XCTestCase
+
+@end
+
+@implementation _3ThreadsTestHW02Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
